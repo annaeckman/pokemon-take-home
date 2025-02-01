273dd2c682be7fcf2173bcb6b4ecabbdb2973e32
annaeckman <annaeckman@icloud.com>
2025-01-31 16:54:24 -0800

    basic setup for fetching, storing list in state, and displaying full list

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,43 @@
-import { useState } from "react";
-import reactLogo from "./assets/react.svg";
-import viteLogo from "/vite.svg";
+import { useState, useEffect } from "react";
+
 import "./App.css";
 
 function App() {
-  const [count, setCount] = useState(0);
+  const [pokemonList, setPokemonList] = useState([]);
+  const [searchTerm, setSearchTerm] = useState("");
+
+  //when site renders, get the list of all pokemon and store in state
+  useEffect(() => {
+    fetch("https://pokeapi.co/api/v2/pokemon?limit=1500")
+      .then((res) => res.json())
+      .then((json) => setPokemonList(json.results.map(({ name }) => name)));
+  }, []);
+
+  //filter the list based on the search term
+  const filteredPokemonList = pokemonList.filter((name) => {
+    name.toLowerCase().includes(searchTerm.toLowerCase());
+  });
 
   return (
     <>
-      <h2 className="text-3xl font-bold underline">Testing!</h2>
+      <h2 className="text-3xl font-bold underline">Pokemon</h2>
+      <label htmlFor="search"></label>
+      <input
+        className="w-full bg-blue-200 p-4 rounded-sm"
+        id="search"
+        name="search"
+        type="text"
+        placeholder="search for your favorite pokemon"
+        value={searchTerm}
+        onChange={(e) => setSearchTerm(e.target.value)}
+      />
+      <ul className="grid gap-4">
+        {pokemonList.map((name, index) => (
+          <li key={index} className="p-2 border rounded-md">
+            {name}
+          </li>
+        ))}
+      </ul>
     </>
   );
 }
